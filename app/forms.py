2a449d74ed014f59ddc393874379ff8f9e38706d
Romarie Haughton <romariehaughton@gmail.com>
2025-03-16 14:32:37 +0800

    Updated login route with authentication

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from flask_wtf.file import FileField, FileRequired
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired
-
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired()])
     password = PasswordField('Password', validators=[InputRequired()])
+
+
+class UploadForm(FlaskForm):  # Add this missing form
+    file = FileField('File', validators=[FileRequired()])
+    submit = SubmitField('Upload')
